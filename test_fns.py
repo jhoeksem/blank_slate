f85d7a264c39d82167b6df695988ab8d48acfae8
jhoeksem <34220557+jhoeksem@users.noreply.github.com>
2020-04-22 10:29:25 -0400

    Create test_fns.py

---
+++ test_fns.py
@@ -0,0 +1,13 @@
+from fns import *
+
+def test_inc():
+    assert inc(3) == 4
+    assert inc(5.5) == 6.5
+
+def test_greet():
+    assert greet("John") == "Hello John!"
+    assert greet("") == "Hello !"
+
+def test_square():
+    assert square(5) == 25
+    assert square(2.5) == 6.25
